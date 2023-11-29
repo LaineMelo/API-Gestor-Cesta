c65614e8f83f157766c421374f7545665326eb06
Elaine Melo <elaine.melo.1353338@sga.pucminas.br>
2023-11-29 13:11:46 -0300

    deploy 01

---
+++ gestor-cestas-api/Migrations/20231129142136_M00.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace gestor_cestas_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class M00 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Beneficiarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Apelido = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RG = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CPF = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Telefone = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Logradouro = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Bairro = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Numero = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Cidade = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Estado = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Beneficiarios", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Dependentes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdBeneficiario = table.Column<int>(type: "int", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Parentesco = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CPF = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dependentes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ListaNecessidades",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdBeneficiario = table.Column<int>(type: "int", nullable: false),
+                    ListaNecessidades = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ListaNecessidades", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RegistroCesta",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdBeneficiario = table.Column<int>(type: "int", nullable: false),
+                    IdVoluntario = table.Column<int>(type: "int", nullable: false),
+                    QuantidadeCesta = table.Column<int>(type: "int", nullable: false),
+                    DataEntrega = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RegistroCesta", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Voluntarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CPF = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Logradouro = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Bairro = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Numero = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Cidade = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Voluntarios", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LinkDto",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Href = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Rel = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Metodo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BeneficiarioId = table.Column<int>(type: "int", nullable: true),
+                    VoluntarioId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LinkDto", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LinkDto_Beneficiarios_BeneficiarioId",
+                        column: x => x.BeneficiarioId,
+                        principalTable: "Beneficiarios",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_LinkDto_Voluntarios_VoluntarioId",
+                        column: x => x.VoluntarioId,
+                        principalTable: "Voluntarios",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LinkDto_BeneficiarioId",
+                table: "LinkDto",
+                column: "BeneficiarioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LinkDto_VoluntarioId",
+                table: "LinkDto",
+                column: "VoluntarioId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Dependentes");
+
+            migrationBuilder.DropTable(
+                name: "LinkDto");
+
+            migrationBuilder.DropTable(
+                name: "ListaNecessidades");
+
+            migrationBuilder.DropTable(
+                name: "RegistroCesta");
+
+            migrationBuilder.DropTable(
+                name: "Beneficiarios");
+
+            migrationBuilder.DropTable(
+                name: "Voluntarios");
+        }
+    }
+}
--- gestor-cestas-api/Program.cs
+++ gestor-cestas-api/Program.cs
@@ -6,8 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -16,16 +17,22 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseCors();
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+app.UseSwagger();
 
-app.MapControllers();
+app.UseSwaggerUI((c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Gestor Cesta API v1");
+}));
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
 
 app.Run();
